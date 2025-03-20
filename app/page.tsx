6e14f80bbe4c58fd29c005098fdacdffdd2eee35
Shaun-Allan <shaunallan0605@gmail.com>
2025-03-20 10:44:54 +0530

    hello

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,19 @@
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import Accordion from "./components/Accordion";
+import CircularText from "./components/CircularText";
 
-
-
-import ScrollVelocity from './components/ScrollVelocity';
+import ScrollVelocity from "./components/ScrollVelocity";
 import { Download } from "lucide-react";
 
-import BlurText from './components/BlurText'
+import BlurText from "./components/BlurText";
 
 export default function Home() {
   const [scaled, setScaled] = useState(false);
 
-
   const handleAnimationComplete = () => {
-    console.log('Animation completed!');
+    console.log("Animation completed!");
   };
-
 
   useEffect(() => {
     const handleScroll = () => {
@@ -33,8 +30,11 @@ export default function Home() {
     <>
       <div className="p-0 m-0">
         <div
-          className={`h-screen w-full relative overflow-hidden flex items-center justify-center transition-all duration-500 ${scaled ? "scale-93 sm:scale-97 rounded-lg" : "scale-100 rounded-none"
-            }`}
+          className={`h-screen w-full relative overflow-hidden flex items-center justify-center transition-all duration-500 ${
+            scaled
+              ? "scale-93 sm:scale-97 rounded-lg"
+              : "scale-100 rounded-none"
+          }`}
         >
           {/* Video Background */}
           <video
@@ -158,15 +158,19 @@ export default function Home() {
 
         {/* Pledge Section */}
 
-
-
-
         <div className="relative h-screen w-full px-6 sm:px-14 py-20 flex flex-col gap-10 justify-center items-center">
-  {/* Background Overlay */}
-  <div className="absolute inset-0 bg-[url('/ai-bg-4.avif')] bg-cover bg-center opacity-25"></div>
+          {/* Background Overlay */}
+          <div className="absolute inset-0 bg-[url('/ai-bg-4.avif')] bg-cover bg-center opacity-25"></div>
 
-  {/* Content */}
-  {/* <div className="relative z-10"> */}
+          <CircularText
+            text="A R T I F I C I A L * I N T E L L I G E N C E * "
+            onHover="speedUp"
+            spinDuration={20}
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          />
+
+          {/* Content */}
+          {/* <div className="relative z-10"> */}
           <BlurText
             text="AI For All"
             delay={150}
@@ -183,30 +187,42 @@ export default function Home() {
             onAnimationComplete={handleAnimationComplete}
             className="font-[Merriweather] font-bold  text-6xl sm:text-9xl mb-2 sm:mb-8"
           />
-        {/* </div> */}
+          {/* </div> */}
         </div>
 
-
-
         <ScrollVelocity
-          texts={["Embracing AI in Education |", "Shaping India\'s Future |"]}
+          texts={["Embracing AI in Education |", "Shaping India's Future |"]}
           velocity={50}
           className="custom-scroll-text"
         />
 
         {/* Spacer Section */}
         <div className="min-h-full sm:min-h-screen w-full px-6 sm:px-40 pt-20 sm:pt-0 sm:py-20 flex flex-col sm:flex-row gap-15 justify-center items-center">
-          <div className="font-[Inter] text-justify sm:text-start text-lg sm:text-xl leading-[1.8] sm:leading-[2.5]" style={{ wordSpacing: "4px" }}>The AI Student Chapter is dedicated to fostering innovation, learning, and collaboration in the field of Artificial Intelligence. Aligned with <span className="text-3xl sm:text-4xl font-bold font-[Merriweather]">AICTE</span>&apos;s (All India Council for Technical Education) vision, we aim to integrate AI into education through workshops, research initiatives, industry partnerships, and student-driven projects. Our mission is to equip students with the knowledge and skills needed to lead the AI revolution, ensuring ethical and impactful advancements in technology.<a
-            href="/AI Student Chapter.pdf"
-            download="AI Student Chapter.pdf"
+          <div
+            className="font-[Inter] text-justify sm:text-start text-lg sm:text-xl leading-[1.8] sm:leading-[2.5]"
+            style={{ wordSpacing: "4px" }}
           >
-            <i className="fa-solid fa-up-right-from-square ml-2"></i>
-          </a></div>
+            The AI Student Chapter is dedicated to fostering innovation,
+            learning, and collaboration in the field of Artificial Intelligence.
+            Aligned with{" "}
+            <span className="text-3xl sm:text-4xl font-bold font-[Merriweather]">
+              AICTE
+            </span>
+            &apos;s (All India Council for Technical Education) vision, we aim
+            to integrate AI into education through workshops, research
+            initiatives, industry partnerships, and student-driven projects. Our
+            mission is to equip students with the knowledge and skills needed to
+            lead the AI revolution, ensuring ethical and impactful advancements
+            in technology.
+            <a href="/AI Student Chapter.pdf" download="AI Student Chapter.pdf">
+              <i className="fa-solid fa-up-right-from-square ml-2"></i>
+            </a>
+          </div>
           <div className="relative rounded-md overflow-hidden shadow-xl group hidden sm:inline">
             {/* Image */}
             <Image
-            height={400}
-            width={400}
+              height={400}
+              width={400}
               src="/aicte.png"
               alt="aicte"
               className="rounded-md w-[1400px] h-auto shadow-xl transition-transform duration-300 group-hover:scale-110"
@@ -223,13 +239,13 @@ export default function Home() {
               </a>
             </div>
           </div>
-
-
         </div>
 
         {/* Spacer Section */}
         <div className="min-h-full w-full px-6 sm:px-14 pb-20 flex flex-col gap-10 mt-20 sm:mt-0">
-          <span className="font-[Merriweather] font-semibold text-3xl sm:text-5xl">Questions Related To Our Club</span>
+          <span className="font-[Merriweather] font-semibold text-3xl sm:text-5xl">
+            Questions Related To Our Club
+          </span>
           <Accordion></Accordion>
         </div>
       </div>
